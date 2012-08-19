1a7397a829f51ad43e189636f01f3280c852e00f
Juan C. Müller <jcmuller@gmail.com>
2012-08-19 12:50:25 -0400

    App generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabyrinthSolver::Application.config.secret_token = '94542359a970de585e1b6d07287c3b6370e5bb78aa8f169eb14c5a586e902bedc9da6594dc61754f6eeabab3d545c13aaa610b1bb97bd0c0d99d2421c0d8dc90'
